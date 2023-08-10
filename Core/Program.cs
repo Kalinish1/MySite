bbafae28dc30d8da2beb0fe868bd6886616ff853
Kalinish1 <87180551+Kalinish1@users.noreply.github.com>
2023-08-10 13:08:06 +0300

    Connected PostgreSQL db

--- Core/Program.cs
+++ Core/Program.cs
@@ -1,7 +1,14 @@
+using Core.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
