1fcd7bff65212968aa8cddf41bc881bf89bd91b1
Kalinish <www.vol@yandex.ru>
2023-08-21 12:23:27 +0300

    Created Upsert in product controller instead of Create and Edit

--- Core/Areas/Admin/Controllers/ProductController.cs
+++ Core/Areas/Admin/Controllers/ProductController.cs
@@ -22,7 +22,7 @@ namespace Core.Web.Areas.Admin.Controllers
             var objectProductList = _unitOfWork.Product.GetAll().ToList();
             return View(objectProductList);
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             //преобразуем список в SelectListItem для создания dropdown 
             //ViewBag.CategoryList = CategoryList;
@@ -37,11 +37,22 @@ namespace Core.Web.Areas.Admin.Controllers
                     }),
                 Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                //create
+                return View(productVM);
+            }
+            else
+            {
+                //update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+            
         }
 
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (productVM.Product.Title != null && productVM.Product.Title == productVM.Product.Description.ToString())
             { //custom validation
@@ -66,32 +77,6 @@ namespace Core.Web.Areas.Admin.Controllers
                 return View(productVM);
             }
             
-        }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var product = _unitOfWork.Product.Get(c => c.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid) // server validation (from model data annotations)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index", "Product");
-            }
-            return View();
         }
         public IActionResult Delete(int? id)
         {
