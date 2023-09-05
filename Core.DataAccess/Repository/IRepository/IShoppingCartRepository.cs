5189bed2ab6c667c0027da90a85ce0f73094f738
Kalinish <www.vol@yandex.ru>
2023-09-05 15:55:13 +0300

    Added ShoppingCart to Db

---
+++ Core.DataAccess/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Repository.IRepository
+{
+    public interface IShoppingCartRepository : IRepository<ShoppingCart>
+    {
+        void Update(ShoppingCart shoppingCart);
+    }
+}
--- Core.DataAccess/Repository/UnitOfWork.cs
+++ Core.DataAccess/Repository/UnitOfWork.cs
@@ -14,6 +14,8 @@ namespace Core.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; set; }
+        public IShoppingCartRepository ShoppingCart { get; set; }
+
         private ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -21,6 +23,8 @@ namespace Core.DataAccess.Repository
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+
         }
         public void Save()
         {
